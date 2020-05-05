1842cd1d0357ae04d70f762ffc7b0f515319ab7f
Michael Naramore <naramore@gmail.com>
2020-05-05 21:09:45 +0000

    more notes

--- apps/domino/lib/domino.ex
+++ apps/domino/lib/domino.ex
@@ -2,22 +2,39 @@ defmodule Domino do
   # NOTE: rename this (at some point?)
   # see https://github.com/domino-clj/domino
   
+  # NOTE: I LIKE the name domino, but what do we call a single
+  #       schema? domino (singular)? that is less 'intuitive'
+  # NOTE: rename schema? (i.e. the collection of model, events, effects, etc.)
+  
+  # nested schemas, multi-schemas
+  
   # MODEL:
   #   - support streaming / partitioning?
   #   - nesting triggers and/or interceptors
+  #   - default root trigger (w/ id=root)
+  #   - root interceptors
   #   - ids
-  #   - sequences / collections of data
+  #   - enumerable: element_id?
+  #   - collection: key=id
+  #   - external / remote?
+  #   - nested 'schema'
   # INPUTS/OUTPUTS:
   #   - omitted / unchanged results
   #   - revisiting nodes (use interceptors? allow / disallow cycles?)
   #   - wait for multiple triggers (join?) using interceptors?
   # HANDLERS:
+  #   - interceptors attached to handlers
   #   - domino-clj args :: [ctx inputs outputs]
   #     args :: [ctx] (???)
   #   - step
-  # INTERCEPTORS: use interceptor app
+  #   - error handling?
+  #   - events vs effects (i.e. side-effects)
   # HISTORY:
   #   - graph-like history (i.e. track origin and parent of each 'event')
+  #   - linear (execution order) history
   # ENGINE:
   #   - deterministic/intuitive execution path w/ multiple inputs/outputs
+  #   - multiple outputs present an 'ordering' problem (i.e. which to execute 1st?)
+  #   - multiple inputs present a 'waiting' problem (i.e. should we wait for potential changes, or re-execute on each/first input change)
+  #   - engine protocol / behaviour? + depth-first, breadth-first, async engines
 end
--- apps/interceptor/lib/interceptor.ex
+++ apps/interceptor/lib/interceptor.ex
@@ -2,4 +2,33 @@ defmodule Interceptor do
   # see http://pedestal.io/reference/interceptors
   # see https://lispcast.com/a-model-of-interceptors/
   # see https://github.com/exoscale/interceptor
+  # see https://github.com/metosin/sieppari
+  
+  # 'unrecoverable' interceptors:
+  # enter, leave, error
+  # a -> b -> c
+  # => enter a -> enter b -> enter c |-> leave c -> leave b -> leave a
+  #                                  |-> error c -> error b -> error a
+  #                                  |
+  #                          'point of no return'
+  # => enter b -> error -> leave b (i.e. short-circuit)
+  # => leave b -> error -> error b -> error|leave a
+  
+  # error
+  # halt
+  # terminate
+  # enqueue, inject
+  # when
+  # in, out, lens
+  # discard
+  
+  # 'recoverable' interceptors (worth it?)
+  # would involve leave and/or error to conditionally point back to enter?
+  # does this even make sense for interceptors?
+  # saga: txn -> ok
+  #           -> cmp
+  #       cmp -> error
+  #           -> abort
+  #           -> txn
+  #           -> ok
 end
